9a0474146a78d792972e4f657d2918dacc5e77bd
Stephen James <78153905+ItsMeSteve88@users.noreply.github.com>
2023-06-23 14:30:15 +0100

    navbar mediaquery and positioning

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
         <Navbar />
         <div className="body bodypos">
            <div className="sidebar sidebarpos"></div>
-           <div className="main">right
+           <div className="main mainpos">right
               <div className='bg-orange-300 h- md:w-[416px] md:h-[709px] '>
                  todays highlights
               </div>

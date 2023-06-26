4a1c5467611d3f1a267c7e3cc66a15193a6b4a52
Stephen James <78153905+ItsMeSteve88@users.noreply.github.com>
2023-06-26 17:37:23 +0100

    layout tbd

--- src/components/main/HpvfCard.jsx
+++ src/components/main/HpvfCard.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const HpvfCard = ({title, percent, hPa, km, degrees, icon}) => {
   return (
-     <div className="w-[248px] h-[106px] md:w-[174px] md:h-[120px] p-4 gap-5 bg-base-200 rounded-2xl flex flex-col items-start self-stretch">
+     <div className="w-[248px] md:w-[174px] lg:w-[320px] h-[106px] md:h-[120px] lg:h-[150px] p-4 gap-5 bg-base-200 rounded-2xl flex flex-col items-start self-stretch">
         <h1 className='text-base font-semibold'>{title}</h1>
         <div className='flex justify-between items-center self-stretch'>
            {icon}

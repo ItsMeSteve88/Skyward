59a5006ae3a79aa6c9dc5a32c1a170866830873e
Stephen James <78153905+ItsMeSteve88@users.noreply.github.com>
2023-06-27 15:00:33 +0100

    livin on a prayer

--- src/components/main/AqiCard.jsx
+++ src/components/main/AqiCard.jsx
@@ -29,7 +29,23 @@ const AqiCard = () => {
                      </div>
                   </div>
                </div>
-               <div></div>
+               <div className='flex items-start self-stretch'>
+                  <div className='flex justify-end items-center gap-1 flex-1'>
+                     <p className='text-xl'>0.3</p>
+                     <div className='flex items-end'>
+                        <p className='text-sm'>NO</p>
+                        <p className='text-xs'>2</p>
+                     </div>
+                  </div>
+                  <div className='flex justify-end items-center gap-1 flex-1'>
+                     <p className='text-xl'>75.1</p>
+                     <div className='flex items-end'>
+                        <p className='text-sm'>O</p>
+                        <p className='text-xs'>3</p>
+                     </div>
+                  </div>
+               </div>
+               
             </div>
          </div>
      </>
